6879bacfcfa427cec5f8bd64052c1378161cc55c
rcmdnk <rcmdnk@gmail.com>
2013-09-21 03:22:28 +0200

    resetTunnel.sh, from darwin11

--- resetTunnel.sh
+++ resetTunnel.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 mkdir -p ~/tmp
-ps -u ${USER}|grep /usr/bin/ssh > ~/tmp/.tmptunnel
+ps -u ${USER}|grep ssh|grep -v agent|grep -v sshd |grep -v grep> ~/tmp/.tmptunnel
 while read LINE;do
+  echo $LINE
   TTY=`echo ${LINE}|awk '{print $5}'`
+  echo $TTY
   if [ ${TTY} = '?' ];then
     PID=`echo ${LINE}|awk '{print $1}'`
+    echo $PID
     kill -kill ${PID}
   fi
 done < ~/tmp/.tmptunnel
