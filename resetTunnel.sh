ef636a1b51d0f8873cc07f4d9db0af01689e12df
rcmdnk <rcmdnk@gmail.com>
2019-08-26 03:02:30 +0900

    LICENSE    README.md    install.sh    resetTunnel.sh, from darwin18

--- resetTunnel.sh
+++ resetTunnel.sh
@@ -2,10 +2,8 @@
 mkdir -p ~/tmp
 ps -u ${USER}|grep ssh|grep -v agent|grep -v sshd |grep -v grep> ~/tmp/.tmptunnel
 while read LINE;do
-  echo $LINE
   TTY=`echo ${LINE}|awk '{print $5}'`
-  echo $TTY
-  if [ ${TTY} = '?' ];then
+  if [ "${TTY}" = '?' ];then
     PID=`echo ${LINE}|awk '{print $1}'`
     echo $PID
     kill -kill ${PID}
